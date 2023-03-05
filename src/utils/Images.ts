9e8babebdab4bdf684dabd2d27431edf9ac17aa6
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-03-05 17:05:45 -0300

    image: add downArrow

--- src/utils/Images.ts
+++ src/utils/Images.ts
@@ -2,3 +2,4 @@ const baseImagePath =
 	"https://raw.githubusercontent.com/jairo-sousa/boracodar-9-currency/main/.github/";
 
 export const direction = `${baseImagePath}direction.svg`;
+export const downArrow = `${baseImagePath}downArrow.svg`;
