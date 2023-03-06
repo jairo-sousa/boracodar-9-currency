5398660b0c66177157882493432af3de12f3612b
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-03-05 21:22:00 -0300

    style: set ExchangeRate

--- src/components/ExchangeRate.tsx
+++ src/components/ExchangeRate.tsx
@@ -1,3 +1,13 @@
+import { Flex, Image } from "@chakra-ui/react";
+import { ContainerWrapper } from "./ContainerWrapper";
+import { chart } from "../utils/Images";
+
 export function ExchangeRate() {
-	return <p>Exchange rate</p>;
+	return (
+		<Flex direction="column" gap="2.4rem">
+			<ContainerWrapper title="Exchange rate">
+				<Image src={chart} alt="chart" />
+			</ContainerWrapper>
+		</Flex>
+	);
 }
